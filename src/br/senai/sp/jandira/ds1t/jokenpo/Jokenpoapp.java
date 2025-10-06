0792d4b5392005c0efc1e091e87b3a0c0c78dddd
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2025-10-06 16:18:47 -0300

    jokenpo pronto iniciando função de recomeçar

--- src/br/senai/sp/jandira/ds1t/jokenpo/Jokenpoapp.java
+++ src/br/senai/sp/jandira/ds1t/jokenpo/Jokenpoapp.java
@@ -1,8 +1,13 @@
 package br.senai.sp.jandira.ds1t.jokenpo;
 
+import br.senai.sp.jandira.ds1t.jokenpo.model.JokenpoSistema;
+
 public class Jokenpoapp {
     public static void main (String[] args){
         System.out.println("Iniciando...");
+
+        JokenpoSistema media = new JokenpoSistema();
+        media.coletarDados();
 
     }
 }
--- src/br/senai/sp/jandira/ds1t/jokenpo/model/JokenpoSistema.java
+++ src/br/senai/sp/jandira/ds1t/jokenpo/model/JokenpoSistema.java
@@ -1,12 +1,14 @@
 package br.senai.sp.jandira.ds1t.jokenpo.model;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class JokenpoSistema {
 
-    int 1
-    int 2
-    int 3
+    int valorDoUsuario;
+    int valorDoComputador;
+
+
 
 
     public void coletarDados(){
@@ -15,15 +17,83 @@ public class JokenpoSistema {
         System.out.println("----------------------------------------------------");
         System.out.println("*** VAMOS INICIAR O JOKENPÔ ***");
         System.out.println("----------------------------------------------------");
-
         System.out.println("Escolha uma opção:");
         System.out.println("(1) Pedra");
         System.out.println("(2) Papel");
         System.out.println("(3) Tesoura");
         System.out.println("----------------------------------------------------");
-        System.out.print("Digite sua escolha:");
+        System.out.println("Digite sua escolha: ");
+        valorDoUsuario = leitor.nextInt();
 
+        convencionarNumeroEmOpcoes();
+    }
+
+
+    public void convencionarNumeroEmOpcoes(){
+
+        if (valorDoUsuario == 1){
+            System.out.println("Você escolheu: Pedra");
+
+        } else if (valorDoUsuario == 2) {
+            System.out.println("Você escolheu: Papel");
+
+        } else if (valorDoUsuario == 3){
+            System.out.println("Você escolheu: Tesoura");
+        }
+            ativarEscolhaDoComputador();
+    }
+
+    public void ativarEscolhaDoComputador(){
+        Random random = new Random();
+
+        valorDoComputador = random.nextInt(3)+1;
+        transformarEscolhaDoComputador();
 
 
     }
+    public void transformarEscolhaDoComputador(){
+
+        if (valorDoComputador == 1){
+            System.out.println("O computador escolheu: Pedra");
+
+        } else if (valorDoComputador == 2){
+            System.out.println("O computador escolheu: Papel");
+
+        } else if (valorDoComputador == 3) {
+            System.out.println("O computador escolheu: Tesoura");
+
+        }
+
+        exibirResultados();
+
+    }
+
+    public void exibirResultados(){
+
+        if (valorDoComputador == valorDoUsuario){
+            System.out.println("Empate");
+
+        } else if (valorDoComputador == 1 && valorDoUsuario == 2) {
+            System.out.println("Você venceu");
+
+        } else if (valorDoComputador == 1 && valorDoUsuario == 3) {
+            System.out.println("Você perdeu");
+
+        } else if (valorDoComputador == 2 && valorDoUsuario == 1) {
+            System.out.println("Você perdeu");
+
+        } else if (valorDoComputador == 2 && valorDoUsuario == 3) {
+            System.out.println("Você venceu");
+
+        } else if (valorDoComputador == 3 && valorDoUsuario == 1) {
+            System.out.println("Você venceu");
+
+        } else if (valorDoComputador == 3 && valorDoUsuario == 2) {
+            System.out.println("Você perdeu");
+
+        }
+
+    }
+
+
 }
