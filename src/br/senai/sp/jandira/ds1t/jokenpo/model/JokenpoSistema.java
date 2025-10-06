97553d59fc3826d8c39ba0797851dc721d7fae8f
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2025-10-06 13:33:34 -0300

    apresentação das opções

--- src/br/senai/sp/jandira/ds1t/jokenpo/model/JokenpoSistema.java
+++ src/br/senai/sp/jandira/ds1t/jokenpo/model/JokenpoSistema.java
@@ -1,4 +1,29 @@
 package br.senai.sp.jandira.ds1t.jokenpo.model;
 
+import java.util.Scanner;
+
 public class JokenpoSistema {
+
+    int 1
+    int 2
+    int 3
+
+
+    public void coletarDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("----------------------------------------------------");
+        System.out.println("*** VAMOS INICIAR O JOKENPÔ ***");
+        System.out.println("----------------------------------------------------");
+
+        System.out.println("Escolha uma opção:");
+        System.out.println("(1) Pedra");
+        System.out.println("(2) Papel");
+        System.out.println("(3) Tesoura");
+        System.out.println("----------------------------------------------------");
+        System.out.print("Digite sua escolha:");
+
+
+
+    }
 }
